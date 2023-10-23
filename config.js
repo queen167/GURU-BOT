c638ffe44256b1246886b3aab674ba0057632520
queen167 <145977630+queen167@users.noreply.github.com>
2023-10-23 13:02:21 +0500

    Update config.js

--- config.js
+++ config.js
@@ -50,10 +50,28 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '꧁•⊹٭𝙽𝚘 𝚙𝚖 😡
+
+
+𖤍
+
+
+𖤍'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = '꧁•⊹٭𝙽𝚘 𝚙𝚖 😡
+
+
+𖤍
+
+
+𖤍' 
+global.author = '꧁•⊹٭𝙽𝚘 𝚙𝚖 😡
+
+
+𖤍
+
+
+𖤍' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
